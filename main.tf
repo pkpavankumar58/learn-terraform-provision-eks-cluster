47d66a0d576114573282235275a0fa6caf76fed9
pkpavankumar58 <110784930+pkpavankumar58@users.noreply.github.com>
2024-02-03 16:09:04 +0530

    Update main.tf
    added node group

--- main.tf
+++ main.tf
@@ -63,17 +63,26 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 3
-      desired_size = 2
+      desired_size = 1
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t2.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+    three = {
+      name = "node-group-3"
+
+      instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 2
